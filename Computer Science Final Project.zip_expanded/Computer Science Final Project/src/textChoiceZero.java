fe9732d362263abae96b40385562dd5649df4fe1
Evan Kenyon <evan.m.kenyon@gmail.com>
2017-11-12 15:50:03 -0500

    test

---
+++ Computer Science Final Project.zip_expanded/Computer Science Final Project/src/textChoiceZero.java
@@ -0,0 +1,69 @@
+
+public class textChoiceZero extends textChoice{
+	
+	/**
+	 * This method contains the intro text
+	 * @return returns a string containing the intro text
+	 */
+	public static String introText() {
+		String str = "Beep. Beep. Beep."
+				+ "	"
+				+ "	Good morning Chicago! It's your favorite radio host, Tom Hanks, coming to you live"
+				+ " from WFM 92.4. The time is now 6 o'clock. It's looking like it's gonna be a sunny day today, with a"
+				+ " low of 65 and a high of 80 --"
+				+"	"
+				+ "	You hit the button on your alarm clock. You look next to you to see your"
+				+ " wife, the woman of your dreams. You can never tell her what you really do. It would upset her too much,"
+				+ " and you'd definitely lose custody of the kids. You love them so much --  Keightylynn, Mustafa and"
+				+ " Gaylord. Your wife came up with the names."
+				+ "	"
+				+ "	You push yourself off of the bed. Time to get the day"
+				+ " started. People underestimate how challenging it is to raise a family of five in the suburbs of"
+				+ " Chicago. Life was hard at first, your financial trouble just became too much. And things only got"
+				+ " worse when Keightylnn was born. You had no other choice. It started off as just a few stolen items"
+				+ " here, a few drug hussles there. And now look at you. You're now leader of the Bloods in Chicago."
+				+ "	"
+				+ "	You"
+				+ " freshen up and get ready for the day, today is an especially big day. You go down to the garage, the"
+				+ " one place your wife hardly visits. You approach your \"toolbox\" and fiddle with the rusted lock. It"
+				+ " pops open to reveal your most trusted tools -- a rifle, a handgun, a few blades, and several other"
+				+ " miscellaneous items. Now, which one should you take to work today...";
+		return str;
+	}
+	
+	/**
+	 * sets the 1st choice A
+	 * @return the first choice A
+	 */
+	public static String choiceA() {
+		String a = "Pick up the rifle";
+		return a;
+	}
+	
+	/**
+	 * sets the 1st choice B
+	 * @return the first choice B
+	 */
+	public static String choiceB() {
+		String b = "Pick up the handgun";
+		return b;
+	}
+	
+	/**
+	 * sets the 1st choice C
+	 * @return the first choice C
+	 */
+	public static String choiceC() {
+		String c = "Pick up the knife";
+		return c;
+	}
+	
+	/**
+	 * sets the 1st choice D
+	 * @return the first choice D
+	 */
+	public static String choiceD() {
+		String d = "Don't pick up a weapon";
+		return d;
+	}
+}
